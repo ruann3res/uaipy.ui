268e4a2d7bdfd2d3848b45a6f003b163dfd07797
ruann3res <ruanpablo1218@gmail.com>
2025-08-27 15:19:11 -0300

    feat: reports

--- src/pages/Reports/ReportGraph.tsx
+++ src/pages/Reports/ReportGraph.tsx
@@ -150,7 +150,7 @@ export function ReportGraph() {
           </>
         )}
       </div>
-      <div className="grid grid-cols-1 gap-6 mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {selectedSensors.map((sensorId) => {
           const selectedSensor = sensorData?.find(
             (s: SensorData) => s.sensor_id === sensorId
